f80243ceaa4335d22e291204a12f270c900d456e
Ashley Plasket <ashley.plasket@gmail.com>
2024-01-15 14:06:43 -0500

    add ; to end of function and return

--- src/components/Food.tsx
+++ src/components/Food.tsx
@@ -8,13 +8,12 @@ function getRandomFruitEmoji(){
 };
 
 export default function Food({ x, y }: Coordinate): JSX.Element {
-
   return (
     <Text style={[{top: y * 10, left: x * 10}, styles.food]}>
       🍎
     </Text>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   food: {
